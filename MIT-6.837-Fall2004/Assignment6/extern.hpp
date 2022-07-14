0a2fd044faaa2fadba51d9076872386abe7c7e09
++ <lsjiajia1209@gmail.com>
2022-07-14 10:08:19 +0800

    Assignment6

---
+++ MIT-6.837-Fall2004/Assignment6/extern.hpp
@@ -0,0 +1,16 @@
+#ifndef extern_hpp
+#define extern_hpp
+
+#include <stdio.h>
+
+#pragma once
+
+extern int width;
+extern int height;
+extern bool shade_back;
+extern int theta_steps;
+extern int phi_steps;
+extern bool gouraud;
+extern bool visualize_grid;
+
+#endif /* extern_hpp */
