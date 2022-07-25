d02f3ebef2220cc921133d314ab96db3da7c5ac2
++ <lsjiajia1209@gmail.com>
2022-07-25 11:35:57 +0800

    update

--- MIT-6.837-Fall2004/Assignment0/DerivedData/Assignment0/Build/Intermediates.noindex/Assignment0.build/Debug/Assignment0.build/Objects-normal/x86_64/image.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/liushijia/Desktop/➵C:C++/C++/Assignment0/Assignment0/image.cpp \
-  /Users/liushijia/Desktop/➵C:C++/C++/Assignment0/Assignment0/image.hpp \
-  /Users/liushijia/Desktop/➵C:C++/C++/Assignment0/Assignment0/vectors.hpp
--- MIT-6.837-Fall2004/Assignment0/DerivedData/Assignment0/Build/Intermediates.noindex/Assignment0.build/Debug/Assignment0.build/Objects-normal/x86_64/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/liushijia/Desktop/➵C:C++/C++/Assignment0/Assignment0/main.cpp \
-  /Users/liushijia/Desktop/➵C:C++/C++/Assignment0/Assignment0/image.hpp \
-  /Users/liushijia/Desktop/➵C:C++/C++/Assignment0/Assignment0/vectors.hpp \
-  /Users/liushijia/Desktop/➵C:C++/C++/Assignment0/Assignment0/ifs.hpp \
-  /Users/liushijia/Desktop/➵C:C++/C++/Assignment0/Assignment0/matrix.hpp
